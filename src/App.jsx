77b6af1173b96dbdfce4fc9a42abc98e13310353
deletebackspace <hoanpv076@gmail.com>
2025-04-09 22:10:16 +0700

    Tao grid layout

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <div className="w-full h-full">
+        <div className="grid grid-cols-12">
+          <div className="col-span-3">
+
+          </div>
+          <div className="col-span-9">
+
+          </div>
+        </div>
+      </div>
+
+    </>
+  )
+}
+
+export default App
